094671211a81f615e1466387f78b6c4417ad706b
lannwyd <daliardodali@gmail.com>
2026-08-02 22:46:13 +0100

    added generation

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from sentence_transformers import SentenceTransformer
 from numpy import dot , linalg
+import os
+from dotenv import load_dotenv 
+from groq import Groq
 
+load_dotenv()
 
+API_KEY = os.getenv("GROQ_API_KEY")
+client = Groq()
 
 model = SentenceTransformer("all-MiniLM-L6-v2") 
 
 N = 500
 text =""
 
+with open("data/data.txt", "r", encoding="utf-8") as file:
+    text = file.read()
 
 def chunk_text(text : str , chunk_size : int ) -> list[str]:
     chunks = []
@@ -34,18 +42,52 @@ def retrieve(query : str , chunks : list[str] , model , top_k = 3) :
         vector = model.encode(chunk)
         numbers.append((cosim_similarity(question_vector , vector) , chunk))
     numbers.sort(reverse=True)
-    results = numbers[0:top_k]
+    results = numbers[0:top_k ]
     return results
 
 def cosim_similarity(chunkonevec , chunktwovec) :
     return dot(chunkonevec , chunktwovec)/(linalg.norm(chunkonevec) * linalg.norm(chunktwovec))
 
-with open("data/data.txt", "r", encoding="utf-8") as file:
-    text = file.read()
+def generate_answer(question , context) :
+    client = Groq()
+
+    stream = client.chat.completions.create(
+        messages=[
+            
+            {
+                "role": "system",
+                "content": "Answer the question using ONLY the context below. If the answer isn't in the context, say you don't know.\n\n context:\n" + context,
+            },
+            {
+                "role": "user",
+                "content": question
+            }
+            
+        ],
+        model="openai/gpt-oss-120b",
+
+        temperature=0.5,
+
+        max_completion_tokens=1024,
+
+        top_p=1,
+
+        stop=None,
+
+        stream=True,
+    )
+    for chunk in stream:
+        content = chunk.choices[0].delta.content
+        if content is not None:
+                print(chunk.choices[0].delta.content, end="")
 
 chunks = chunk_text(text,N)
-
+    
 qst = input("ask your question :\n")
-
 top = retrieve(qst , chunks ,model)
-print(top)
+
+cominedchunks = ""
+for number , chunk in top:
+    cominedchunks += chunk
+
+generate_answer(qst , cominedchunks)
